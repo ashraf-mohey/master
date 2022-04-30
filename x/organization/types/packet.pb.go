e810dafd1ee178a72dd237b99ed8095b0ad42cd8
Ashraf Mohey <ashraf.mohey2000@gmail.com>
2022-04-30 13:40:15 +0200

    Added ibc packet

--- x/organization/types/packet.pb.go
+++ x/organization/types/packet.pb.go
@@ -25,6 +25,7 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 type OrganizationPacketData struct {
 	// Types that are valid to be assigned to Packet:
 	//	*OrganizationPacketData_NoData
+	//	*OrganizationPacketData_IbcOrganizationPacket
 	Packet isOrganizationPacketData_Packet `protobuf_oneof:"packet"`
 }
 
@@ -70,8 +71,12 @@ type isOrganizationPacketData_Packet interface {
 type OrganizationPacketData_NoData struct {
 	NoData *NoData `protobuf:"bytes,1,opt,name=noData,proto3,oneof" json:"noData,omitempty"`
 }
+type OrganizationPacketData_IbcOrganizationPacket struct {
+	IbcOrganizationPacket *IbcOrganizationPacketData `protobuf:"bytes,2,opt,name=ibcOrganizationPacket,proto3,oneof" json:"ibcOrganizationPacket,omitempty"`
+}
 
-func (*OrganizationPacketData_NoData) isOrganizationPacketData_Packet() {}
+func (*OrganizationPacketData_NoData) isOrganizationPacketData_Packet()                {}
+func (*OrganizationPacketData_IbcOrganizationPacket) isOrganizationPacketData_Packet() {}
 
 func (m *OrganizationPacketData) GetPacket() isOrganizationPacketData_Packet {
 	if m != nil {
@@ -87,10 +92,18 @@ func (m *OrganizationPacketData) GetNoData() *NoData {
 	return nil
 }
 
+func (m *OrganizationPacketData) GetIbcOrganizationPacket() *IbcOrganizationPacketData {
+	if x, ok := m.GetPacket().(*OrganizationPacketData_IbcOrganizationPacket); ok {
+		return x.IbcOrganizationPacket
+	}
+	return nil
+}
+
 // XXX_OneofWrappers is for the internal use of the proto package.
 func (*OrganizationPacketData) XXX_OneofWrappers() []interface{} {
 	return []interface{}{
 		(*OrganizationPacketData_NoData)(nil),
+		(*OrganizationPacketData_IbcOrganizationPacket)(nil),
 	}
 }
 
@@ -130,28 +143,133 @@ func (m *NoData) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_NoData proto.InternalMessageInfo
 
+// IbcOrganizationPacketData defines a struct for the packet payload
+type IbcOrganizationPacketData struct {
+	Mnemonic string `protobuf:"bytes,1,opt,name=mnemonic,proto3" json:"mnemonic,omitempty"`
+	Creator  string `protobuf:"bytes,2,opt,name=creator,proto3" json:"creator,omitempty"`
+}
+
+func (m *IbcOrganizationPacketData) Reset()         { *m = IbcOrganizationPacketData{} }
+func (m *IbcOrganizationPacketData) String() string { return proto.CompactTextString(m) }
+func (*IbcOrganizationPacketData) ProtoMessage()    {}
+func (*IbcOrganizationPacketData) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8898018af4a5e866, []int{2}
+}
+func (m *IbcOrganizationPacketData) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *IbcOrganizationPacketData) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_IbcOrganizationPacketData.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *IbcOrganizationPacketData) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_IbcOrganizationPacketData.Merge(m, src)
+}
+func (m *IbcOrganizationPacketData) XXX_Size() int {
+	return m.Size()
+}
+func (m *IbcOrganizationPacketData) XXX_DiscardUnknown() {
+	xxx_messageInfo_IbcOrganizationPacketData.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_IbcOrganizationPacketData proto.InternalMessageInfo
+
+func (m *IbcOrganizationPacketData) GetMnemonic() string {
+	if m != nil {
+		return m.Mnemonic
+	}
+	return ""
+}
+
+func (m *IbcOrganizationPacketData) GetCreator() string {
+	if m != nil {
+		return m.Creator
+	}
+	return ""
+}
+
+// IbcOrganizationPacketAck defines a struct for the packet acknowledgment
+type IbcOrganizationPacketAck struct {
+	Address string `protobuf:"bytes,1,opt,name=address,proto3" json:"address,omitempty"`
+}
+
+func (m *IbcOrganizationPacketAck) Reset()         { *m = IbcOrganizationPacketAck{} }
+func (m *IbcOrganizationPacketAck) String() string { return proto.CompactTextString(m) }
+func (*IbcOrganizationPacketAck) ProtoMessage()    {}
+func (*IbcOrganizationPacketAck) Descriptor() ([]byte, []int) {
+	return fileDescriptor_8898018af4a5e866, []int{3}
+}
+func (m *IbcOrganizationPacketAck) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *IbcOrganizationPacketAck) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_IbcOrganizationPacketAck.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *IbcOrganizationPacketAck) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_IbcOrganizationPacketAck.Merge(m, src)
+}
+func (m *IbcOrganizationPacketAck) XXX_Size() int {
+	return m.Size()
+}
+func (m *IbcOrganizationPacketAck) XXX_DiscardUnknown() {
+	xxx_messageInfo_IbcOrganizationPacketAck.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_IbcOrganizationPacketAck proto.InternalMessageInfo
+
+func (m *IbcOrganizationPacketAck) GetAddress() string {
+	if m != nil {
+		return m.Address
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*OrganizationPacketData)(nil), "ashrafmohey.master.organization.OrganizationPacketData")
 	proto.RegisterType((*NoData)(nil), "ashrafmohey.master.organization.NoData")
+	proto.RegisterType((*IbcOrganizationPacketData)(nil), "ashrafmohey.master.organization.IbcOrganizationPacketData")
+	proto.RegisterType((*IbcOrganizationPacketAck)(nil), "ashrafmohey.master.organization.IbcOrganizationPacketAck")
 }
 
 func init() { proto.RegisterFile("organization/packet.proto", fileDescriptor_8898018af4a5e866) }
 
 var fileDescriptor_8898018af4a5e866 = []byte{
-	// 193 bytes of a gzipped FileDescriptorProto
+	// 284 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0xcc, 0x2f, 0x4a, 0x4f,
 	0xcc, 0xcb, 0xac, 0x4a, 0x2c, 0xc9, 0xcc, 0xcf, 0xd3, 0x2f, 0x48, 0x4c, 0xce, 0x4e, 0x2d, 0xd1,
 	0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x92, 0x4f, 0x2c, 0xce, 0x28, 0x4a, 0x4c, 0xcb, 0xcd, 0xcf,
-	0x48, 0xad, 0xd4, 0xcb, 0x4d, 0x2c, 0x2e, 0x49, 0x2d, 0xd2, 0x43, 0x56, 0xad, 0x94, 0xca, 0x25,
-	0xe6, 0x8f, 0xc4, 0x0f, 0x00, 0x6b, 0x76, 0x49, 0x2c, 0x49, 0x14, 0x72, 0xe4, 0x62, 0xcb, 0xcb,
-	0x07, 0xb1, 0x24, 0x18, 0x15, 0x18, 0x35, 0xb8, 0x8d, 0xd4, 0xf5, 0x08, 0x98, 0xa5, 0xe7, 0x07,
-	0x56, 0xee, 0xc1, 0x10, 0x04, 0xd5, 0xe8, 0xc4, 0xc1, 0xc5, 0x06, 0x71, 0x8d, 0x12, 0x07, 0x17,
-	0x1b, 0x44, 0xd6, 0xc9, 0xf7, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92,
-	0x63, 0x9c, 0xf0, 0x58, 0x8e, 0xe1, 0xc2, 0x63, 0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0xa2, 0x8c,
-	0xd3, 0x33, 0x4b, 0x32, 0x4a, 0x93, 0xf4, 0x92, 0xf3, 0x73, 0xf5, 0x21, 0x56, 0xe9, 0x82, 0xed,
-	0xd2, 0x87, 0xd8, 0xa5, 0x5f, 0xa1, 0x8f, 0xe2, 0xcf, 0x92, 0xca, 0x82, 0xd4, 0xe2, 0x24, 0x36,
-	0xb0, 0x3f, 0x8d, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0x2c, 0xce, 0x38, 0x3f, 0x04, 0x01, 0x00,
-	0x00,
+	0x48, 0xad, 0xd4, 0xcb, 0x4d, 0x2c, 0x2e, 0x49, 0x2d, 0xd2, 0x43, 0x56, 0xad, 0x74, 0x93, 0x91,
+	0x4b, 0xcc, 0x1f, 0x49, 0x20, 0x00, 0xac, 0xdb, 0x25, 0xb1, 0x24, 0x51, 0xc8, 0x91, 0x8b, 0x2d,
+	0x2f, 0x1f, 0xc4, 0x92, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x36, 0x52, 0xd7, 0x23, 0x60, 0x98, 0x9e,
+	0x1f, 0x58, 0xb9, 0x07, 0x43, 0x10, 0x54, 0xa3, 0x50, 0x11, 0x97, 0x68, 0x66, 0x52, 0x32, 0xa6,
+	0xf9, 0x12, 0x4c, 0x60, 0x13, 0xad, 0x08, 0x9a, 0xe8, 0x89, 0x4d, 0x37, 0xd4, 0x12, 0xec, 0x46,
+	0x3b, 0x71, 0x70, 0xb1, 0x41, 0x82, 0x40, 0x89, 0x83, 0x8b, 0x0d, 0xe2, 0x22, 0xa5, 0x40, 0x2e,
+	0x49, 0x9c, 0x26, 0x09, 0x49, 0x71, 0x71, 0xe4, 0xe6, 0xa5, 0xe6, 0xe6, 0xe7, 0x65, 0x26, 0x83,
+	0x7d, 0xca, 0x19, 0x04, 0xe7, 0x0b, 0x49, 0x70, 0xb1, 0x27, 0x17, 0xa5, 0x26, 0x96, 0xe4, 0x17,
+	0x81, 0x9d, 0xcc, 0x19, 0x04, 0xe3, 0x2a, 0x99, 0x70, 0x49, 0x60, 0x35, 0xd2, 0x31, 0x39, 0x1b,
+	0xa4, 0x2b, 0x31, 0x25, 0xa5, 0x28, 0xb5, 0xb8, 0x18, 0x6a, 0x20, 0x8c, 0xeb, 0xe4, 0x7b, 0xe2,
+	0x91, 0x1c, 0xe3, 0x85, 0x47, 0x72, 0x8c, 0x0f, 0x1e, 0xc9, 0x31, 0x4e, 0x78, 0x2c, 0xc7, 0x70,
+	0xe1, 0xb1, 0x1c, 0xc3, 0x8d, 0xc7, 0x72, 0x0c, 0x51, 0xc6, 0xe9, 0x99, 0x25, 0x19, 0xa5, 0x49,
+	0x7a, 0xc9, 0xf9, 0xb9, 0xfa, 0x90, 0x50, 0xd1, 0x05, 0x07, 0x8b, 0x3e, 0x24, 0x58, 0xf4, 0x2b,
+	0xf4, 0x51, 0x62, 0xb9, 0xa4, 0xb2, 0x20, 0xb5, 0x38, 0x89, 0x0d, 0x1c, 0xcb, 0xc6, 0x80, 0x00,
+	0x00, 0x00, 0xff, 0xff, 0x43, 0x95, 0x32, 0x3c, 0x02, 0x02, 0x00, 0x00,
 }
 
 func (m *OrganizationPacketData) Marshal() (dAtA []byte, err error) {
@@ -207,6 +325,27 @@ func (m *OrganizationPacketData_NoData) MarshalToSizedBuffer(dAtA []byte) (int, 
 	}
 	return len(dAtA) - i, nil
 }
+func (m *OrganizationPacketData_IbcOrganizationPacket) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *OrganizationPacketData_IbcOrganizationPacket) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	if m.IbcOrganizationPacket != nil {
+		{
+			size, err := m.IbcOrganizationPacket.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintPacket(dAtA, i, uint64(size))
+		}
+		i--
+		dAtA[i] = 0x12
+	}
+	return len(dAtA) - i, nil
+}
 func (m *NoData) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
@@ -227,6 +366,73 @@ func (m *NoData) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
+	return len(dAtA) - i, nil
+}
+
+func (m *IbcOrganizationPacketData) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *IbcOrganizationPacketData) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *IbcOrganizationPacketData) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Creator) > 0 {
+		i -= len(m.Creator)
+		copy(dAtA[i:], m.Creator)
+		i = encodeVarintPacket(dAtA, i, uint64(len(m.Creator)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.Mnemonic) > 0 {
+		i -= len(m.Mnemonic)
+		copy(dAtA[i:], m.Mnemonic)
+		i = encodeVarintPacket(dAtA, i, uint64(len(m.Mnemonic)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *IbcOrganizationPacketAck) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *IbcOrganizationPacketAck) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *IbcOrganizationPacketAck) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Address) > 0 {
+		i -= len(m.Address)
+		copy(dAtA[i:], m.Address)
+		i = encodeVarintPacket(dAtA, i, uint64(len(m.Address)))
+		i--
+		dAtA[i] = 0xa
+	}
 	return len(dAtA) - i, nil
 }
 
@@ -265,12 +471,54 @@ func (m *OrganizationPacketData_NoData) Size() (n int) {
 	}
 	return n
 }
+func (m *OrganizationPacketData_IbcOrganizationPacket) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.IbcOrganizationPacket != nil {
+		l = m.IbcOrganizationPacket.Size()
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	return n
+}
 func (m *NoData) Size() (n int) {
 	if m == nil {
 		return 0
 	}
 	var l int
 	_ = l
+	return n
+}
+
+func (m *IbcOrganizationPacketData) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Mnemonic)
+	if l > 0 {
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	l = len(m.Creator)
+	if l > 0 {
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	return n
+}
+
+func (m *IbcOrganizationPacketAck) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Address)
+	if l > 0 {
+		n += 1 + l + sovPacket(uint64(l))
+	}
 	return n
 }
 
@@ -344,6 +592,41 @@ func (m *OrganizationPacketData) Unmarshal(dAtA []byte) error {
 			}
 			m.Packet = &OrganizationPacketData_NoData{v}
 			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field IbcOrganizationPacket", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			v := &IbcOrganizationPacketData{}
+			if err := v.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			m.Packet = &OrganizationPacketData_IbcOrganizationPacket{v}
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipPacket(dAtA[iNdEx:])
@@ -394,6 +677,202 @@ func (m *NoData) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: NoData: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipPacket(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *IbcOrganizationPacketData) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowPacket
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: IbcOrganizationPacketData: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: IbcOrganizationPacketData: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Mnemonic", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Mnemonic = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Creator", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Creator = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipPacket(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *IbcOrganizationPacketAck) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowPacket
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: IbcOrganizationPacketAck: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: IbcOrganizationPacketAck: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Address", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Address = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := skipPacket(dAtA[iNdEx:])
